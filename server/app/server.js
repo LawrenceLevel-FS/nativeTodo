74da3fb0b5fc75b9f5b49a905f973ae64ad7c9df
LawrenceLevel-FS <141098091+LawrenceLevel-FS@users.noreply.github.com>
2024-07-28 16:40:31 -0400

    connected database

--- server/app/server.js
+++ server/app/server.js
@@ -4,10 +4,14 @@ const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
+const db = require("./db/database");
 
 // middleware
 app.use(morgan("dev"));
 app.use(cors());
+
+// Database start
+db();
 
 // setting port
 const port = process.env.PORT || 3001;
