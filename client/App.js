4e1ffd55988220ab8cea8e9e94593b0b1bba0bad
LawrenceLevel-FS <141098091+LawrenceLevel-FS@users.noreply.github.com>
2024-07-28 22:59:22 -0400

    set up state management

--- client/App.js
+++ client/App.js
@@ -1,21 +1,32 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //importing pages
 import { SignIn } from "./app/pages/SignIn";
 import { Todos } from "./app/pages/Todos";
+import { AuthProvider } from "./app/components/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // signin checker
+  useEffect(() => {
+    const user = false;
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Todos" component={Todos} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="Todos" component={Todos} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
--- client/app/pages/SignIn.js
+++ client/app/pages/SignIn.js
@@ -1,12 +1,24 @@
-import { Text, View, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button, Alert } from "react-native";
+import { AuthContext } from "../components/AuthContext";
+import { useContext } from "react";
 
-export function SignIn({ navigation }) {
+export function SignIn({ navigation, route }) {
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
+  setCurrentUser(false);
+  const handleSignIn = () => {
+    if (currentUser) {
+      navigation.navigate("Todos");
+      console.log(currentUser);
+    } else {
+      Alert.alert("Please sign in");
+    }
+  };
   return (
     <View>
       <Text>sign in</Text>
       <TextInput name="email" placeholder="email" />
       <TextInput name="password" placeholder="password" />
-      <Button onPress={() => navigation.navigate("Todos")} title="Todo Page" />
+      <Button onPress={handleSignIn} title="Sign in " />
     </View>
   );
 }
